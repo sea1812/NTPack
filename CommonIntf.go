f07d0b817a70f987f6bb376ea4ffa4c98560218f
jiang <jiang5630@outlook.com>
2025-07-29 15:20:49 +0800

    Create CommonIntf.go

---
+++ CommonIntf.go
@@ -0,0 +1,27 @@
+/**--------------------------------------------------------
+ * Author: jiang5630@outlook.com 2025-07-29
+ * Description:
+ * This file is part of the NTPack project.
+ * --------------------------------------------------------
+ * 作者：jiang5630@outlook.com  2025年07月29日
+ * 描述：通用类型定义
+ --------------------------------------------------------*/
+
+package NTPack
+
+import (
+	"time"
+)
+
+// TCBComponentHeader 组件头信息结构
+type TCBComponentHeader struct {
+	ComponentId  string    //组件名
+	PublisherID  string    //发布消息的署名
+	Version      string    //版本号
+	Intro        string    //介绍
+	Author       string    //作者名称
+	StartTime    time.Time //启动时间
+	SnowID       string    //雪花ID
+	ServerNodeId int64     //服务器节点ID
+	Pid          int       //进程ID
+}
